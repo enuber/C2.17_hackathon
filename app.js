a9d24bc53177936fe04b15ba246466fb96b4fe35
Sean Mee <Sean Mee>
2017-03-22 15:40:09 -0700

    food pairings ajax call added

--- app.js
+++ app.js
@@ -1,17 +1,39 @@
-$.ajax({
-    data: {
-        url: 'http://api.brewerydb.com/v2/styles?key=075d4da050ae5fd39db3ded4fd982c92'
-    },
-    url: "serverProxy/proxy.php",
-    method: "POST",
-    dataType: 'json',
-    success: function(result){
-        for (var i=0; i<result.data.length; i++) {
-            console.log(result.data[i].shortName);
+// function callBeerStyle() {
+//     $.ajax({
+//         data: {
+//             url: 'http://api.brewerydb.com/v2/styles?key=075d4da050ae5fd39db3ded4fd982c92'
+//         },
+//         url: "serverProxy/proxy.php",
+//         method: "GET",
+//         dataType: 'json',
+//         success: function (result) {
+//             for (var i = 0; i < result.data.length; i++) {
+//                 console.log(result.data[i].shortName);
+//             }
+//         },
+//         error: function () {
+//             console.log('error');
+//         }
+//     });
+// }
+
+function callFoodPairings() {
+    $.ajax({
+        data: {
+            url: 'http://api.brewerydb.com/v2/beers?key=075d4da050ae5fd39db3ded4fd982c92&name=Imperial%20IPA'
+        },
+        url: 'serverProxy/proxy.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function (result) {
+            for (var i = 0; i < result.data.length; i++) {
+                if (result.data[i].foodPairings !== undefined) {
+                    console.log(result.data[i].foodPairings);
+                }
+            }
+        },
+        error: function () {
+            console.log('error')
         }
-    },
-    error: function(){
-        console.log('error');
-    }
-});
-//http://api.brewerydb.com/v2/styles
+    });
+}
