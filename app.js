45e175687f5a37414182bd9054fc6bffd9426389
Sean Mee <Sean Mee>
2017-03-22 18:31:22 -0700

    update

--- app.js
+++ app.js
@@ -110,26 +110,8 @@ $(document).ready(function(){
     startUp();
 });
 
-// function callBeerStyle() {
-//     $.ajax({
-//         data: {
-//             url: 'http://api.brewerydb.com/v2/styles?key=075d4da050ae5fd39db3ded4fd982c92'
-//         },
-//         url: "serverProxy/proxy.php",
-//         method: "GET",
-//         dataType: 'json',
-//         success: function (result) {
-//             for (var i = 0; i < result.data.length; i++) {
-//                 console.log(result.data[i].shortName);
-//             }
-//         },
-//         error: function () {
-//             console.log('error');
-//         }
-//     });
-// }
 
-//
+var foodPairings;
 function callFoodPairings() {
     var beerSelected = $('input:checked').val();
     $.ajax({
@@ -142,7 +124,7 @@ function callFoodPairings() {
         success: function (result) {
             for (var i = 0; i < result.data.length; i++) {
                 if (result.data[i].foodPairings !== undefined) {
-                    console.log(result.data[i].foodPairings);
+                    foodPairings = result.data[i].foodPairings;
                 }
             }
         },
