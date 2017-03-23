1ed47454a64c6d4a2cfa61b7aada13b6042dc3a8
Sean Mee <Sean Mee>
2017-03-22 21:08:46 -0700

    Styling added

--- app.js
+++ app.js
@@ -26,59 +26,7 @@ function initialize() {
             position: google.maps.ControlPosition.TOP_RIGHT
         }
     });
-    //brian create search box and link it to the element
-    // var input = document.getElementById('pac-input');
-    // var searchBox = new google.maps.places.SearchBox(input);
-    // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    // map.addListener('bounds_changed', function(){
-    //     searchBox.setBounds(map.getBounds());
-    // });
-    // searchBox.addListener('places_changed', function(){
-    //     var places = searchBox.getPlaces();
-    //
-    //     if (places.length == 0) {
-    //         return;
-    //     }
-    //
-    //     // Clear out the old markers.
-    //     markers.forEach(function(marker) {
-    //         marker.setMap(null);
-    //     });
-    //     markers = [];
-    //
-    //     // For each place, get the icon, name and location.
-    //     var bounds = new google.maps.LatLngBounds();
-    //     places.forEach(function(place) {
-    //         if (!place.geometry) {
-    //             console.log("Returned place contains no geometry");
-    //             return;
-    //         }
-    //         // var icon = {
-    //         //     url: place.icon,
-    //         //     size: new google.maps.Size(71, 71),
-    //         //     origin: new google.maps.Point(0, 0),
-    //         //     anchor: new google.maps.Point(17, 34),
-    //         //     scaledSize: new google.maps.Size(25, 25)
-    //         // };
-    //
-    //         // Create a marker for each place.
-    //         markers.push(new google.maps.Marker({
-    //             map: map,
-    //             // icon: icon,
-    //             title: place.name,
-    //             position: place.geometry.location
-    //         }));
-    //
-    //         if (place.geometry.viewport) {
-    //             // Only geocodes have viewport.
-    //             bounds.union(place.geometry.viewport);
-    //         } else {
-    //             bounds.extend(place.geometry.location);
-    //         }
-    //     });
-    //     map.fitBounds(bounds);
-    // });
-    //end brian autocomplete code, trouble with geolocation
+
     request = {
         location: center,
         radius: 8047,
@@ -200,14 +148,17 @@ function callFoodPairings() {
     });
 }
 function submitBeerSelection(){
-    $('#modalContainer').css('display','none');
+    // $('#modalContainer').css('display','none');
+    $('#domContainer').html('');
     $('#beginSearch').css('display','initial');
     callFoodPairings();
+    setTimeout(foodPairingDomCreation,1400);
 
 }
 function findYourBeerInit(){
     $('#modalContainer').css('display','initial');
     $('#beginSearch').css('display','none');
+    $('#domContainer').html('');
 }
 function applyClickHandlers(){
     $('#submitBeerButton').click(submitBeerSelection);
@@ -215,5 +166,14 @@ function applyClickHandlers(){
     $(".currentLoc").click(getLocation);
     $(".submit").click(codeAddress);
 }
+function foodPairingDomCreation(){
+    var $div = $('<div>',{
+       text: foodPairings,
+       class: "domFoodPair"
+    });
+    $('#domContainer').append($div);
+}
 
+var imageContainer = {
 
+}
